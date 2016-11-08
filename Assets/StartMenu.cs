01dc65a3c1cd810a66e76cb609346991c4f050b4
Matt Crowder <mcrowder65@gmail.com>
2016-11-08 15:48:19 -0700

    maybe i'll crash eric

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -5,7 +5,7 @@ public class StartMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -19,6 +19,6 @@ public class StartMenu : MonoBehaviour {
     {
         //TODO
 
-        Debug.Log("It worked!");
+        Debug.Log("penis");
     }
 }
