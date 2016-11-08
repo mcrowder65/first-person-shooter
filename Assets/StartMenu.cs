9688433e1a159dd3e9ec275dde07dcb44494ad17
Eric <soreric94@gmail.com>
2016-11-08 15:37:18 -0700

    first commit

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    public void EnterGameButton_Clicked()
+    {
+        //TODO
+
+        Debug.Log("It worked!");
+    }
+}
