7a23f8bd37186a7e37a44f79621537627da5adf0
Matt Crowder <mcrowder65@gmail.com>
2016-11-11 16:44:28 -0700

    square better

--- Assets/SquareScript.cs
+++ Assets/SquareScript.cs
@@ -7,12 +7,41 @@ public class SquareScript : MonoBehaviour {
 	{
 		
 	}
-	void FixedUpdate()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+//	void Update()
+//	{
+//		if (Input.GetKeyDown (KeyCode.D)) {
+//			transform.Rotate(Vector3.right * Time.deltaTime);
+//		}
+//		if (Input.GetKeyDown (KeyCode.A)) {
+//			transform.Rotate(Vector3.left * Time.deltaTime);
+//		}
+//	}
+//	void FixedUpdate()
+//	{
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+//
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//		transform.Translate(movement);
+//	}
+	public float moveSpeed = 10f;
+	public float turnSpeed = 50f;
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		transform.Translate(movement);
+
+	void Update ()
+	{
+		if(Input.GetKey(KeyCode.UpArrow))
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.DownArrow))
+			transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+		if (Input.GetKeyUp (KeyCode.J))
+			transform.Translate (new Vector3 (0, 1, 0));
 	}
 }
