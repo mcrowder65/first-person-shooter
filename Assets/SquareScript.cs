5d7d8d6bca227312bfe3bd3d97e4602f9e7b9542
Matt Crowder <mcrowder65@gmail.com>
2016-11-11 15:52:41 -0700

    working on moving

---
+++ Assets/SquareScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareScript : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int movespeed = 1;
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody> ();
+		Debug.Log ("Start!");
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement);
+
+	}
+}
