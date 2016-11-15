50573a30816fb4e31670d3e089362448e48feec5
Matt Crowder <mcrowder65@gmail.com>
2016-11-15 14:50:46 -0700

    baymax has camera positioned properly, and he moves around wellish

--- Assets/BaymaxMovement.cs
+++ Assets/BaymaxMovement.cs
@@ -1,57 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BaymaxMovement : MonoBehaviour {
+public class BaymaxMovement : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
-    public float moveSpeed = 30f;
-    public float turnSpeed = 50f;
-    void moveForward()
-    {
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-    }
-    void moveBackwards()
-    {
-        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-    }
-    void rotateLeft()
-    {
-        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-    }
-    void rotateRight()
-    {
-        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-    }
-    // Update is called once per frame
-    void Update () {
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            moveForward();
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            moveBackwards();
-        }
+	public float moveSpeed = 30f;
+	public float turnSpeed = 50f;
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rotateLeft();
-        }
+	void moveForward ()
+	{
+		Vector3 vect = new Vector3 (-1, 0, 0);
+		transform.Translate (vect * moveSpeed * Time.deltaTime);
+	}
+
+	void moveBackwards ()
+	{
+		Vector3 vect = new Vector3 (1, 0, 0);
+		transform.Translate (vect * moveSpeed * Time.deltaTime);
+	}
+
+	void rotateLeft ()
+	{
+		transform.Rotate (Vector3.up, -turnSpeed * Time.deltaTime);
+	}
+
+	void rotateRight ()
+	{
+		transform.Rotate (Vector3.up, turnSpeed * Time.deltaTime);
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			moveForward ();
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			moveBackwards ();
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rotateLeft ();
+		}
             
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rotateRight();
-        }
-        if (Input.GetKeyUp(KeyCode.J))
-            transform.Translate(new Vector3(0, 1, 0));
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Enter called.");
-    }
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rotateRight ();
+		}
+		if (Input.GetKeyUp (KeyCode.J))
+			transform.Translate (new Vector3 (0, 1, 0));
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		Debug.Log ("Enter called.");
+	}
 }
