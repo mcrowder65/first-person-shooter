528e65fb7211bfd6e37cb736a3c27875945c2cea
Matt <mcrowder65@gmail.com>
2016-11-15 18:47:49 -0700

    got xbox controller working!

--- Assets/BaymaxCameraMovement.cs
+++ Assets/BaymaxCameraMovement.cs
@@ -3,33 +3,33 @@ using System.Collections;
 
 public class BaymaxCameraMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
 
     public float speedV = 2.0f;
 
     private float pitch = -1f;
-    // Update is called once per frame
+    // Use this for initialization
+    void Start()
+    {
 
+    }
+    bool isXboxController()
+    {
+        return Input.GetJoystickNames()[0].ToString() == "Controller (XBOX 360 For Windows)";
+    }
     void Update()
     {
         if(pitch == -1f)
         {
             pitch = transform.eulerAngles.x;
         }
-        //TODO remove this if check when making the game live.
-        if (Input.GetMouseButton(0))
-        {
-            pitch -= speedV * Input.GetAxis("Mouse Y");
+        pitch -= speedV * Input.GetAxis(isXboxController() ? "Right joystick vertical" : "Mouse Y");
 
-            transform.eulerAngles = new Vector3(pitch, transform.eulerAngles.y, 0.0f);
-            // Clamp pitch:
-            pitch = Mathf.Clamp(pitch, -90f, 90f);
+        transform.eulerAngles = new Vector3(pitch, transform.eulerAngles.y, 0.0f);
+        // Clamp pitch:
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
 
-            transform.eulerAngles = new Vector3(pitch, transform.eulerAngles.y, 0f);
-       }
+        transform.eulerAngles = new Vector3(pitch, transform.eulerAngles.y, 0f);
         
     }
 }
--- Assets/BaymaxMovement.cs
+++ Assets/BaymaxMovement.cs
@@ -4,38 +4,40 @@ using System.Collections;
 public class BaymaxMovement : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-	{
-	}
+	
 	public float moveSpeed = 30f;
 	public float turnSpeed = 50f;
-	void moveForward ()
+    public float speedH = 2.0f;
+    private float yaw = -1f;
+    private float previousXAxis = 0f;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    bool isXboxController()
+    {
+        return Input.GetJoystickNames()[0].ToString() == "Controller (XBOX 360 For Windows)";
+    }
+    void moveForwardsOrBackwards (float val)
 	{
-		Vector3 vect = new Vector3 (-1, 0, 0);
+		Vector3 vect = new Vector3 (val, 0, 0);
 		transform.Translate (vect * moveSpeed * Time.deltaTime);
 	}
 
-	void moveBackwards ()
-	{
-		Vector3 vect = new Vector3 (1, 0, 0);
-		transform.Translate (vect * moveSpeed * Time.deltaTime);
-	}
-    void moveLeft()
+    void moveLeftOrRight(float val)
     {
-        Vector3 vect = new Vector3(0, 0, -1);
-        transform.Translate(vect * moveSpeed * Time.deltaTime);
+        Vector3 vect = new Vector3(0, 0, val);
+        transform.Translate( vect * moveSpeed * Time.deltaTime);
     }
 
-    void moveRight()
-    {
-        Vector3 vect = new Vector3(0, 0, 1);
-        transform.Translate(vect * moveSpeed * Time.deltaTime);
-    }
     void rotateHorizontally()
     {
-        yaw += speedH * Input.GetAxis("Mouse X");
-
+        if (yaw == -1f)
+        {
+            yaw = transform.eulerAngles.y;
+        }
+        
+        yaw += speedH * Input.GetAxis(isXboxController() ? "Right joystick horizontal" : "Mouse X");
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, yaw, 0.0f);
 
         // Wrap yaw:
@@ -50,47 +52,41 @@ public class BaymaxMovement : MonoBehaviour
         // Set orientation:
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, yaw, 0f);
     }
-    public float speedH = 2.0f;
-
-    private float yaw = -1f;
+    
     
     // Update is called once per frame
     void Update ()
 	{
-        if(yaw == -1f)
+        rotateHorizontally();
+        
+        if(isXboxController())
         {
-            yaw = transform.eulerAngles.y;
+            moveLeftOrRight (Input.GetAxis("Left joystick left right"));
+            moveForwardsOrBackwards(Input.GetAxis("Left joystick forwards backwards"));
         }
-        //TODO remove this check when making the game live
-        if (Input.GetMouseButton(0))
+        else
         {
-            rotateHorizontally();
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveLeftOrRight(-1);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveLeftOrRight(1);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveForwardsOrBackwards(-1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveForwardsOrBackwards(1);
+            }
         }
-        if (Input.GetKey (KeyCode.UpArrow)) {
-			moveForward ();
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			moveBackwards ();
-		}
-
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			moveLeft ();
-		}
-            
-
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			moveRight ();
-		}
+		
 		if (Input.GetKeyUp (KeyCode.J))
 			transform.Translate (new Vector3 (0, 1, 0));
-	}
-    float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360)
-            angle += 360;
-        if (angle > 360)
-            angle -= 360;
-        return Mathf.Clamp(angle, min, max);
+
     }
 
 }
