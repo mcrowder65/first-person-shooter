6b17598517667c1b899e1a9d2d1e13768d858423
Matt <mcrowder65@gmail.com>
2016-11-15 20:48:58 -0700

    finished 5

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Right joystick horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Right joystick vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
