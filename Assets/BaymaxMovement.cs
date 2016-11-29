15c3ff3b857e6bd51953c9f5d7b299bf3dc98608
Matt <mcrowder65@gmail.com>
2016-11-29 11:11:16 -0700

    baymax movement

--- Assets/BaymaxMovement.cs
+++ Assets/BaymaxMovement.cs
@@ -4,113 +4,148 @@ using UnityEngine.Networking;
 public class BaymaxMovement : NetworkBehaviour
 {
 
-	
-	public float moveSpeed = 30f;
-	public float turnSpeed = 50f;
-	public float speedH = 2.0f;
-	private float yaw = -1f;
-	public float speedV = 2.0f;
-	private float pitch = -1f;
-	private Vector3 initialPosition = new Vector3 (4.4f, 53.2f, -70f);
-	// Use this for initialization
-	void Start ()
-	{
-		transform.Translate (initialPosition);
-	}
+
+    public float moveSpeed = 30f;
+    public float turnSpeed = 50f;
+    public float speedH = 2.0f;
+    private float yaw = -1f;
+    public float speedV = 2.0f;
+    private float pitch = -1f;
+    private Vector3 initialPosition = new Vector3(4.4f, 53.2f, -70f);
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            transform.position = initialPosition;
+        }
 
 
-	void moveForwardsOrBackwards (float val)
-	{
-		Vector3 vect = new Vector3 (val, 0, 0);
-		transform.Translate (vect * moveSpeed * Time.deltaTime);
-	}
-
-	void moveLeftOrRight (float val)
-	{
-		Vector3 vect = new Vector3 (0, 0, val);
-		transform.Translate (vect * moveSpeed * Time.deltaTime);
-	}
+    }
 
 
+    void moveForwardsOrBackwards(float val)
+    {
+        Vector3 vect = new Vector3(val, 0, 0);
+        transform.Translate(vect * moveSpeed * Time.deltaTime);
+    }
 
-	void rotateVertically ()
-	{
-		Camera cam = GetComponentInChildren<Camera> ();
-		//TODO figure out how to get this working only for camera :( 
-		if (pitch == -1f) {
-			pitch = cam.transform.eulerAngles.x;
-		}
-		pitch -= speedV * Input.GetAxis (Utilities.isXboxController () ? "Right joystick vertical" : "Mouse Y");
+    void moveLeftOrRight(float val)
+    {
+        Vector3 vect = new Vector3(0, 0, val);
+        transform.Translate(vect * moveSpeed * Time.deltaTime);
+    }
 
-		cam.transform.eulerAngles = new Vector3 (pitch, cam.transform.eulerAngles.y, 0.0f);
-		// Clamp pitch:
-		pitch = Mathf.Clamp (pitch, -90f, 90f);
 
-		cam.transform.eulerAngles = new Vector3 (pitch, cam.transform.eulerAngles.y, 0f);
-	}
 
-	void rotateHorizontally ()
-	{
-		if (yaw == -1f) {
-			yaw = transform.eulerAngles.y;
-		}
-        
-		yaw += speedH * Input.GetAxis (Utilities.isXboxController () ? "Right joystick horizontal" : "Mouse X");
-		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, yaw, 0.0f);
+    void rotateVertically()
+    {
+        Camera cam = GetComponentInChildren<Camera>();
+        //TODO figure out how to get this working only for camera :( 
+        if (pitch == -1f)
+        {
+            pitch = cam.transform.eulerAngles.x;
+        }
+        pitch -= speedV * Input.GetAxis(Utilities.isXboxController() ? "Right joystick vertical" : "Mouse Y");
 
-		// Wrap yaw:
-		while (yaw < 0f) {
-			yaw += 360f;
-		}
-		while (yaw >= 360f) {
-			yaw -= 360f;
-		}
-		// Set orientation:
-		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, yaw, 0f);
-	}
+        cam.transform.eulerAngles = new Vector3(pitch, cam.transform.eulerAngles.y, 0.0f);
+        // Clamp pitch:
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
 
-	void jump ()
-	{
-		transform.Translate (new Vector3 (0, 1, 0));
-	}
-    
-	// Update is called once per frame
-	void Update ()
-	{
-		
-		if (!isLocalPlayer) {
-			GetComponentInChildren<Camera> ().enabled = false;
-			return;
-		}
-		rotateHorizontally ();
-		rotateVertically ();
-		if (Utilities.isXboxController ()) {
-			moveLeftOrRight (Input.GetAxis ("Left joystick left right"));
-			moveForwardsOrBackwards (Input.GetAxis ("Left joystick forwards backwards"));
-			if (Input.GetKeyUp (KeyCode.JoystickButton0)) {
-				jump ();
-			}
-		} else {
-			if (Input.GetKey (KeyCode.A)) {
-				moveLeftOrRight (-1);
-			}
-			if (Input.GetKey (KeyCode.D)) {
-				moveLeftOrRight (1);
-			}
-			if (Input.GetKey (KeyCode.W)) {
-				moveForwardsOrBackwards (-1);
-			}
-			if (Input.GetKey (KeyCode.S)) {
-				moveForwardsOrBackwards (1);
-			}
-			if (Input.GetKeyUp (KeyCode.Space)) {
-				jump ();
-			}
-		}
-		
-		
-			
+        cam.transform.eulerAngles = new Vector3(pitch, cam.transform.eulerAngles.y, 0f);
+    }
 
-	}
+    void rotateHorizontally()
+    {
+        if (yaw == -1f)
+        {
+            yaw = transform.eulerAngles.y;
+        }
+
+        yaw += speedH * Input.GetAxis(Utilities.isXboxController() ? "Right joystick horizontal" : "Mouse X");
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yaw, 0.0f);
+
+        // Wrap yaw:
+        while (yaw < 0f)
+        {
+            yaw += 360f;
+        }
+        while (yaw >= 360f)
+        {
+            yaw -= 360f;
+        }
+        // Set orientation:
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yaw, 0f);
+    }
+
+    void jump()
+    {
+        transform.Translate(new Vector3(0, 1, 0));
+    }
+
+    // Update is called once per frame
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+    void Update()
+    {
+
+        if (!isLocalPlayer)
+        {
+            GetComponentInChildren<Camera>().enabled = false;
+            return;
+        }
+        rotateHorizontally();
+        rotateVertically();
+        if (Utilities.isXboxController())
+        {
+            moveLeftOrRight(Input.GetAxis("Left joystick left right"));
+            moveForwardsOrBackwards(Input.GetAxis("Left joystick forwards backwards"));
+            if (Input.GetKeyUp(KeyCode.JoystickButton0))
+            {
+                jump();
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveLeftOrRight(-1);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveLeftOrRight(1);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveForwardsOrBackwards(-1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveForwardsOrBackwards(1);
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                jump();
+            }
+        }
+
+
+
+
+    }
 
 }
