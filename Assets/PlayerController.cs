8a76afd915b84d276d18b00837fe7b61c3c68ed9
Matt Crowder <mcrowder65@gmail.com>
2016-11-30 10:57:28 -0700

    added baymax to hearth. need to figure out how to set respawn points on each map.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,169 +5,152 @@ public abstract class PlayerController : NetworkBehaviour
 {
 
 
-    public float moveSpeed = 30f;
-    public float turnSpeed = 50f;
-    public float speedH = 2.0f;
-    private float yaw = -1f;
-    public float speedV = 2.0f;
-    private float pitch = -1f;
+	public float moveSpeed = 30f;
+	public float turnSpeed = 50f;
+	public float speedH = 2.0f;
+	private float yaw = -1f;
+	public float speedV = 2.0f;
+	private float pitch = -1f;
 
-    public GameObject bulletPrefab;
-    public Transform bulletSpawn;
-    // Use this for initialization
-    void Start()
-    {
-        if (isLocalPlayer)
-        {
-            transform.position = Utilities.getNewRespawnPoint();
-        }
-    }
-
-
-    void moveForwardsOrBackwards(float val)
-    {
-        Vector3 vect = new Vector3(val, 0, 0);
-        transform.Translate(vect * moveSpeed * Time.deltaTime);
-    }
-
-    void moveLeftOrRight(float val)
-    {
-        Vector3 vect = new Vector3(0, 0, val);
-        transform.Translate(vect * moveSpeed * Time.deltaTime);
-    }
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("start!");
+		if (isLocalPlayer) {
+			Debug.Log ("start!");
+			transform.position = Utilities.getNewRespawnPoint ();
+		}
+	}
 
 
+	void moveForwardsOrBackwards (float val)
+	{
+		Vector3 vect = new Vector3 (val, 0, 0);
+		transform.Translate (vect * moveSpeed * Time.deltaTime);
+	}
 
-    void rotateVertically()
-    {
-        Camera cam = GetComponentInChildren<Camera>();
-        var gun = transform.Find("SubmachineGun");
-        if (pitch == -1f)
-        {
-            pitch = cam.transform.eulerAngles.x;
-        }
-        pitch -= speedV * Input.GetAxis(Utilities.isXboxController() ? "Right joystick vertical" : "Mouse Y");
+	void moveLeftOrRight (float val)
+	{
+		Vector3 vect = new Vector3 (0, 0, val);
+		transform.Translate (vect * moveSpeed * Time.deltaTime);
+	}
 
-        cam.transform.eulerAngles = new Vector3(pitch, cam.transform.eulerAngles.y, 0.0f);
-        gun.transform.eulerAngles = new Vector3(gun.transform.eulerAngles.x, gun.transform.eulerAngles.y, pitch);
-        // Clamp pitch:
-        pitch = Mathf.Clamp(pitch, -90f, 90f);
 
-        cam.transform.eulerAngles = new Vector3(pitch, cam.transform.eulerAngles.y, 0f);
-        gun.transform.eulerAngles = new Vector3(gun.transform.eulerAngles.x, gun.transform.eulerAngles.y, pitch);
-    }
+
+	void rotateVertically ()
+	{
+		Camera cam = GetComponentInChildren<Camera> ();
+		var gun = transform.Find ("SubmachineGun");
+		if (pitch == -1f) {
+			pitch = cam.transform.eulerAngles.x;
+		}
+		pitch -= speedV * Input.GetAxis (Utilities.isXboxController () ? "Right joystick vertical" : "Mouse Y");
+
+		cam.transform.eulerAngles = new Vector3 (pitch, cam.transform.eulerAngles.y, 0.0f);
+		gun.transform.eulerAngles = new Vector3 (gun.transform.eulerAngles.x, gun.transform.eulerAngles.y, pitch);
+		// Clamp pitch:
+		pitch = Mathf.Clamp (pitch, -90f, 90f);
+
+		cam.transform.eulerAngles = new Vector3 (pitch, cam.transform.eulerAngles.y, 0f);
+		gun.transform.eulerAngles = new Vector3 (gun.transform.eulerAngles.x, gun.transform.eulerAngles.y, pitch);
+	}
 
    
-    void rotateHorizontally()
-    {
-        if (yaw == -1f)
-        {
-            yaw = transform.eulerAngles.y;
-        }
+	void rotateHorizontally ()
+	{
+		if (yaw == -1f) {
+			yaw = transform.eulerAngles.y;
+		}
 
-        yaw += speedH * Input.GetAxis(Utilities.isXboxController() ? "Right joystick horizontal" : "Mouse X");
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yaw, 0.0f);
+		yaw += speedH * Input.GetAxis (Utilities.isXboxController () ? "Right joystick horizontal" : "Mouse X");
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, yaw, 0.0f);
 
-        // Wrap yaw:
-        while (yaw < 0f)
-        {
-            yaw += 360f;
-        }
-        while (yaw >= 360f)
-        {
-            yaw -= 360f;
-        }
-        // Set orientation:
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yaw, 0f);
-    }
+		// Wrap yaw:
+		while (yaw < 0f) {
+			yaw += 360f;
+		}
+		while (yaw >= 360f) {
+			yaw -= 360f;
+		}
+		// Set orientation:
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, yaw, 0f);
+	}
 
-    void jump()
-    {
-        transform.Translate(new Vector3(0, 1, 0));
-    }
+	void jump ()
+	{
+		transform.Translate (new Vector3 (0, 1, 0));
+	}
 
-    //public abstract void CmdFire();
-    [Command]
-    public void CmdFire()
-    {
-        // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);
-        // Add velocity to the bullet
-        var canvas = transform.Find("Canvas");
-        var crossHair = canvas.Find("Crosshair");
-        Debug.Log(crossHair);
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 100;
-        bullet.GetComponent<Rigidbody>().position = crossHair.position;
-        // Spawn the bullet on the Clients
-        NetworkServer.Spawn(bullet);
+	//public abstract void CmdFire();
+	[Command]
+	public void CmdFire ()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			             bulletPrefab,
+			             bulletSpawn.position,
+			             bulletSpawn.rotation);
+		// Add velocity to the bullet
+		var canvas = transform.Find ("Canvas");
+		var crossHair = canvas.Find ("Crosshair");
+		Debug.Log (crossHair);
+		bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * 100;
+		bullet.GetComponent<Rigidbody> ().position = crossHair.position;
+		// Spawn the bullet on the Clients
+		NetworkServer.Spawn (bullet);
 
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 1.5f);
-    }
-    
-    void Update()
-    {
+		// Destroy the bullet after 2 seconds
+		Destroy (bullet, 1.5f);
+	}
 
-        if (!isLocalPlayer)
-        {
-            GetComponentInChildren<Camera>().enabled = false;
-            return;
-        }
-        rotateHorizontally();
-        rotateVertically();
-        if (Utilities.isXboxController())
-        {
-            moveLeftOrRight(Input.GetAxis("Left joystick left right"));
-            moveForwardsOrBackwards(Input.GetAxis("Left joystick forwards backwards"));
-            if (Input.GetKeyUp(KeyCode.JoystickButton0))
-            {
-                jump();
-            }
-            float triggerInput = Input.GetAxis("Triggers");
-            //Debug.Log(triggerInput);
-            if (triggerInput > 0)
-            { //left trigger
-                Debug.Log("Grenade!!!");
-            }
-            else if (triggerInput < 0)
-            { //right trigger
-                CmdFire();
-            }
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                moveLeftOrRight(-1);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                moveLeftOrRight(1);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                moveForwardsOrBackwards(-1);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                moveForwardsOrBackwards(1);
-            }
-            if (Input.GetKeyUp(KeyCode.Space))
-            {
-                jump();
-            }
-            if (Input.GetKey(KeyCode.Mouse0))
-            {
-                CmdFire();
-            }
-        }
+	void Update ()
+	{
+
+		if (!isLocalPlayer) {
+			GetComponentInChildren<Camera> ().enabled = false;
+			return;
+		}
+		rotateHorizontally ();
+		rotateVertically ();
+		if (Utilities.isXboxController ()) {
+			moveLeftOrRight (Input.GetAxis ("Left joystick left right"));
+			moveForwardsOrBackwards (Input.GetAxis ("Left joystick forwards backwards"));
+			if (Input.GetKeyUp (KeyCode.JoystickButton0)) {
+				jump ();
+			}
+			float triggerInput = Input.GetAxis ("Triggers");
+			//Debug.Log(triggerInput);
+			if (triggerInput > 0) { //left trigger
+				Debug.Log ("Grenade!!!");
+			} else if (triggerInput < 0) { //right trigger
+				CmdFire ();
+			}
+		} else {
+			if (Input.GetKey (KeyCode.A)) {
+				moveLeftOrRight (-1);
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				moveLeftOrRight (1);
+			}
+			if (Input.GetKey (KeyCode.W)) {
+				moveForwardsOrBackwards (-1);
+			}
+			if (Input.GetKey (KeyCode.S)) {
+				moveForwardsOrBackwards (1);
+			}
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				jump ();
+			}
+			if (Input.GetKey (KeyCode.Mouse0)) {
+				CmdFire ();
+			}
+		}
 
 
 
 
-    }
+	}
 
 }
--- Assets/Utilities.cs
+++ Assets/Utilities.cs
@@ -3,25 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 static class Utilities
 {
-    private static List<Vector3> spawnPositions = new List<Vector3>(
-                                                    new Vector3[] {
-                                                        new Vector3(4.4f, 53.2f, -70f),
-                                                        new Vector3(-2.44f, 70.73669f, -144.27f),
-                                                        new Vector3(88.73848f, 27.75312f, -5.229659f)
-                                                    });
-    private static System.Random rand = new System.Random();
-    public static bool isXboxController()
-    {
-        return Input.GetJoystickNames().Length > 0 && Input.GetJoystickNames()[0].ToString() == "Controller (XBOX 360 For Windows)";
-    }
-    public static Vector3 getNewRespawnPoint()
-    {
-        int min = 0;
-        int max = spawnPositions.Count;
-        int randomIndex = rand.Next(min, max);
+	private static List<Vector3> lockoutSpawnPositions = new List<Vector3> (
+		                                                     new Vector3[] {
+			new Vector3 (4.4f, 53.2f, -70f),
+			new Vector3 (-2.44f, 70.73669f, -144.27f),
+			new Vector3 (88.73848f, 27.75312f, -5.229659f)
+		});
+	private static List<Vector3> hearthSpawnPositions = new List<Vector3> (
+		                                                    new Vector3[] { new Vector3 (312f, 28.5f, 154.2f) 	});
+	private static System.Random rand = new System.Random ();
 
-        return spawnPositions[randomIndex];
-    }
+	public static bool isXboxController ()
+	{
+		return Input.GetJoystickNames ().Length > 0 && Input.GetJoystickNames () [0].ToString () == "Controller (XBOX 360 For Windows)";
+	}
+
+	public static Vector3 getNewRespawnPoint ()
+	{
+		int min = 0;
+		int max = hearthSpawnPositions.Count;
+		int randomIndex = rand.Next (min, max);
+		Debug.Log (hearthSpawnPositions [randomIndex]);
+		return hearthSpawnPositions [randomIndex];
+	}
 }
