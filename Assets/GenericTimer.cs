2fa5e26ec5728434100c49946aac17f5b71ff4e9
Eric <soreric94@gmail.com>
2016-12-01 19:34:58 -0700

    generic timer

---
+++ Assets/GenericTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericTimer  {
+
+    float interval;
+    float timeTrack = 0;
+	
+
+    public void Reset() { timeTrack = 0; }
+    public bool Enabled { get; set; }
+    public bool IncrementIfEnabled()
+    {
+        if (!Enabled) return false;
+        timeTrack += Time.deltaTime;
+        if (timeTrack > interval)
+        {
+            while (timeTrack > interval)
+                timeTrack -= interval;
+            return true;
+        }
+        return false;
+    }
+
+    public GenericTimer(float interval, bool enable)
+    {
+        this.interval = interval;
+        Enabled = enable;
+    }
+}
