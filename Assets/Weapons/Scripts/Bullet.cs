834b0cf4766516b000e778027a75ce1a91c30871
Matt Crowder <mcrowder65@gmail.com>
2016-12-10 19:26:08 -0700

    cant shoot yourself anymore.

--- Assets/Weapons/Scripts/Bullet.cs
+++ Assets/Weapons/Scripts/Bullet.cs
@@ -10,28 +10,27 @@ public class Bullet : Projectile
 	{
        
 	}
-	
+
 	void OnCollisionEnter (Collision collision)
 	{
 		var hit = collision.gameObject;
 
-        if (!IsHittable(hit))
-            return;
+		if (!IsHittable (hit) || owner.transform == hit.transform)
+			return;
        
 
 
 		var health = hit.GetComponent<Health> ();
 		if (health != null) {
-            Debug.Assert(owner != null);
+			Debug.Assert (owner != null);
 			health.TakeDamage (5, owner.transform);
 		}
 		Destroy (gameObject);
 	}
-    public override float Lifespan
-    {
-        get
-        {
-            return 1.5f;
-        }
-    }
+
+	public override float Lifespan {
+		get {
+			return 1.5f;
+		}
+	}
 }
