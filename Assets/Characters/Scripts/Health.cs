68d1f9a4f560cfe8890c3b90585441a62c13674d
Matt Crowder <mcrowder65@gmail.com>
2016-12-12 09:57:20 -0700

    changed network timeout

--- Assets/Characters/Scripts/Health.cs
+++ Assets/Characters/Scripts/Health.cs
@@ -9,111 +9,109 @@ public class Health : NetworkBehaviour
 
 
 
-    [SyncVar(hook = "OnChangeHealth")]
-    public int currentHealth = Constants.MAX_HEALTH;
-    [SyncVar(hook = "OnChangeDeaths")]
-    public int deaths = 0;
-    [SyncVar(hook = "OnChangeKills")]
-    public int kills = 0;
-    public RectTransform healthBar;
+	[SyncVar (hook = "OnChangeHealth")]
+	public int currentHealth = Constants.MAX_HEALTH;
+	[SyncVar (hook = "OnChangeDeaths")]
+	public int deaths = 0;
+	[SyncVar (hook = "OnChangeKills")]
+	public int kills = 0;
+	public RectTransform healthBar;
 
-    public void TakeDamage(int amount, Transform firer)
-    {
-        if (!GetComponentInParent<PlayerController>().Dead)
-        {
-            currentHealth -= amount;
-            if (currentHealth <= 0)
-            {
+	public void TakeDamage (int amount, Transform firer)
+	{
+		if (!GetComponentInParent<PlayerController> ().Dead) {
+			currentHealth -= amount;
+			if (currentHealth <= 0) {
 
-                currentHealth = Constants.MAX_HEALTH;
-                ++deaths;
+				currentHealth = Constants.MAX_HEALTH;
+				++deaths;
 
-                firer.GetComponent<Health>().kills++;
-                RpcDeath(GetComponentInParent<PlayerController>().gameObject);
-            }
-        }
-    }
+				firer.GetComponent<Health> ().kills++;
+				RpcDeath (GetComponentInParent<PlayerController> ().gameObject);
+			}
+		}
+	}
 
-    [Command]
-    public void CmdDeathByFalling()
-    {
-        if (!GetComponentInParent<PlayerController>().Dead)
-        {
-            ++deaths;
-            --kills;
-            RpcDeath(GetComponentInParent<PlayerController>().gameObject);
-        }
-    }
-    void OnChangeDeaths(int deaths) {
-        this.deaths = deaths;
-        updateScoreboard();
-    }
+	[Command]
+	public void CmdDeathByFalling ()
+	{
+		if (!GetComponentInParent<PlayerController> ().Dead) {	
+			++deaths;
+			--kills;
+			RpcDeath (GetComponentInParent<PlayerController> ().gameObject);
+		}
+	}
 
-    void OnChangeKills(int kills)
-    {
-        this.kills = kills;
-        updateScoreboard();
-    }
-    void updateScoreboard()
-    {
-        var baymax = GetComponentInParent<PlayerController>();
-        var canvas = baymax.transform.Find("Canvas");
-        var scoreboard = canvas.transform.Find("Scoreboard");
-        if (deaths > 100)
-        {
-            deaths = 0; // had to hard code this in... idk how it's getting set to 485 randomly...
-        }
-        scoreboard.GetComponent<UnityEngine.UI.Text>().text = "Kills: " + kills + " Deaths: " + deaths;
-    }
-    void OnChangeHealth(int currentHealth)
-    {
-        //TODO: Do we want to set currentHealth here too?
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
-    }
+	void OnChangeDeaths (int deaths)
+	{
+		this.deaths = deaths;
+		updateScoreboard ();
+	}
 
-    [ClientRpc]
-    public void RpcDeath(GameObject subject)
-    {
-        PlayerController controller = subject.GetComponent<PlayerController>();
-        if (!controller.Dead)
-        {
-            controller.Dead = true;
-            controller.GetComponentInChildren<Killcam>().BeginKillcam(this.gameObject);
-            controller.GetComponent<GameAnimator>().DoFallAnimation();
-        }
-    }
+	void OnChangeKills (int kills)
+	{
+		this.kills = kills;
+		updateScoreboard ();
+	}
 
-    [ClientRpc]
+	void updateScoreboard ()
+	{
+		var baymax = GetComponentInParent<PlayerController> ();
+		var canvas = baymax.transform.Find ("Canvas");
+		var scoreboard = canvas.transform.Find ("Scoreboard");
+		if (deaths > 100) {
+			deaths = 0; // had to hard code this in... idk how it's getting set to 485 randomly...
+		}
+		scoreboard.GetComponent<UnityEngine.UI.Text> ().text = "Kills: " + kills + " Deaths: " + deaths;
+	}
+
+	void OnChangeHealth (int currentHealth)
+	{
+		//TODO: Do we want to set currentHealth here too?
+		healthBar.sizeDelta = new Vector2 (currentHealth, healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	public void RpcDeath (GameObject subject)
+	{
+		PlayerController controller = subject.GetComponent<PlayerController> ();
+		if (!controller.Dead) {
+			controller.Dead = true;
+			controller.GetComponentInChildren<Killcam> ().BeginKillcam (this.gameObject);
+			controller.GetComponent<GameAnimator> ().DoFallAnimation ();
+		}
+	}
+
+	[ClientRpc]
 	public void RpcRespawn ()
 	{
 		if (isLocalPlayer) {
-            PlayerController[] allPlayers = GameObject.FindObjectsOfType<PlayerController>();
-            Respawn newRespawn = null;
-            foreach (var p in allPlayers)
-            {
-                if (p.gameObject != gameObject) 
-                    newRespawn = Utilities.getNewRespawnPoint(p.gameObject);
-            }
+			PlayerController[] allPlayers = GameObject.FindObjectsOfType<PlayerController> ();
+			Respawn newRespawn = null;
+			foreach (var p in allPlayers) {
+				if (p.gameObject != gameObject)
+					newRespawn = Utilities.getNewRespawnPoint (p.gameObject);
+			}
 
-            if (newRespawn == null)
-                newRespawn = Utilities.getRandomRespawnPoint();
+			if (newRespawn == null)
+				newRespawn = Utilities.getRandomRespawnPoint ();
 
-            var cam = GetComponent<PlayerController>().myCamera;
-            Debug.Assert(cam != null);
-            var gun = GetComponent<PlayerController>().currentWeapon;
-            Debug.Assert(gun != null);
+			var cam = GetComponent<PlayerController> ().myCamera;
+			Debug.Assert (cam != null);
+			var gun = GetComponent<PlayerController> ().currentWeapon;
+			Debug.Assert (gun != null);
 
-            newRespawn.setRespawn(gameObject, cam.gameObject, gun.gameObject);
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
-            currentHealth = Constants.MAX_HEALTH;
+			newRespawn.setRespawn (gameObject, cam.gameObject, gun.gameObject);
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			currentHealth = Constants.MAX_HEALTH;
 
-        }
-    }
+		}
+	}
 
-    [Command]
-    public void CmdRespawn()
-    {
-        RpcRespawn();
+	[Command]
+	public void CmdRespawn ()
+	{
+		RpcRespawn ();
        
-    }
+	}
 }
--- Assets/Characters/Scripts/PlayerController.cs
+++ Assets/Characters/Scripts/PlayerController.cs
@@ -13,183 +13,180 @@ public abstract class PlayerController : NetworkBehaviour
 	public float speedV = 2.0f;
 	public Transform bulletSpawn;
 
-    public Camera myCamera;
+	public Camera myCamera;
 
 
-    //TODO: THIS IS TEMPORARY
-    public GameObject SubmachinegunPrefab;
-    public GameObject FlamethrowerPrefab;
+	//TODO: THIS IS TEMPORARY
+	public GameObject SubmachinegunPrefab;
+	public GameObject FlamethrowerPrefab;
 
-    public Weapon currentWeapon;
+	public Weapon currentWeapon;
 
 
-    private GenericTimer jumpCooldownTimer = new GenericTimer(0.5f, false);
-    private bool jumpIsCoolingDown = false;
+	private GenericTimer jumpCooldownTimer = new GenericTimer (0.5f, false);
+	private bool jumpIsCoolingDown = false;
 
-    [SyncVar(hook = "OnDeadChanged")]
-    private bool _dead = false;
-    public bool Dead
-    {
-        get { return _dead; }
-        set
-        {
-            _dead = value;
-        }
-    }
-    [SyncVar(hook= "OnWalkingChanged")]
-    private bool _walking = false;
-    public bool Walking
-    {
-        get { return _walking; }
-        protected set
-        {
-            bool old = _walking;
-           // if (old != value)
-            //{
-                _walking = value;
-                if (_walking)
-                    GetComponent<GameAnimator>().BeginWalk(LeftLeg, RightLeg);
-                else
-                    GetComponent<GameAnimator>().EndWalk();
-           // }
-        }
-    }
+	[SyncVar (hook = "OnDeadChanged")]
+	private bool _dead = false;
 
-    public void OnDeadChanged(bool dead)
-    {
-        Dead = dead;
-    }
-    public void OnWalkingChanged(bool walking)
-    {
-        Walking = walking;
-    }
+	public bool Dead {
+		get { return _dead; }
+		set {
+			_dead = value;
+		}
+	}
 
-    //This could be overridden if necessary
-    public GameObject LeftLeg
-    {
-        get
-        {
-            return GetPiece(PieceType.LeftLeg);
-        }
-    }
-    public GameObject RightLeg
-    {
-        get
-        {
-            return GetPiece(PieceType.RightLeg);
-        }
-    }
+	[SyncVar (hook = "OnWalkingChanged")]
+	private bool _walking = false;
 
-    public GameObject GetPiece(PieceType type)
-    {
-        foreach (var p in GetComponentsInChildren<PlayerPiece>())
-        {
-            if (p.TypeOfPiece == type)
-                return p.gameObject;
-        }
-        return null;
-    }
+	public bool Walking {
+		get { return _walking; }
+		protected set {
+			bool old = _walking;
+			// if (old != value)
+			//{
+			_walking = value;
+			if (_walking)
+				GetComponent<GameAnimator> ().BeginWalk (LeftLeg, RightLeg);
+			else
+				GetComponent<GameAnimator> ().EndWalk ();
+			// }
+		}
+	}
+
+	public void OnDeadChanged (bool dead)
+	{
+		Dead = dead;
+	}
+
+	public void OnWalkingChanged (bool walking)
+	{
+		Walking = walking;
+	}
+
+	//This could be overridden if necessary
+	public GameObject LeftLeg {
+		get {
+			return GetPiece (PieceType.LeftLeg);
+		}
+	}
+
+	public GameObject RightLeg {
+		get {
+			return GetPiece (PieceType.RightLeg);
+		}
+	}
+
+	public GameObject GetPiece (PieceType type)
+	{
+		foreach (var p in GetComponentsInChildren<PlayerPiece>()) {
+			if (p.TypeOfPiece == type)
+				return p.gameObject;
+		}
+		return null;
+	}
 
 
 
-    // Use this for initialization
-    public void Start()
-    {
-        updateScoreboard();
-        Physics.gravity = new Vector3(0, -50, 0);
+	// Use this for initialization
+	public void Start ()
+	{
+		updateScoreboard ();
+		Physics.gravity = new Vector3 (0, -50, 0);
 
-        GetComponentInChildren<AudioListener>().enabled = isLocalPlayer;
+		GetComponentInChildren<AudioListener> ().enabled = isLocalPlayer;
 
-        GetComponentInChildren<Killcam>().EndKillcam += PlayerController_EndKillcam;
-        //TODO: THIS IS TEMPORARY
-
-
-
-        GameObject weapon = (GameObject)Instantiate(Utilities.rand.Next(0, 2) == 0 ? SubmachinegunPrefab : FlamethrowerPrefab, this.transform, false);
-        currentWeapon = weapon.GetComponent<Weapon>();
-
-        myCamera = GetComponentInChildren<Camera>();
-
-
-        if (isLocalPlayer)
-        {
-            PlayerController[] allPlayers = GameObject.FindObjectsOfType<PlayerController>();
-            Respawn newRespawn = null;
-            foreach (var p in allPlayers)
-            {
-                if (p.gameObject != gameObject)
-                    newRespawn = Utilities.getNewRespawnPoint(p.gameObject);
-            }
-            if (newRespawn == null)
-                newRespawn = Utilities.getRandomRespawnPoint();
-
-            var cam = myCamera;
-            Debug.Assert(cam != null);
-            var gun = currentWeapon;
-            Debug.Assert(gun != null);
-
-            newRespawn.setRespawn(gameObject, cam.gameObject, gun.gameObject);
+		GetComponentInChildren<Killcam> ().EndKillcam += PlayerController_EndKillcam;
+		//TODO: THIS IS TEMPORARY
 
 
 
-        }
-        gameObject.name = isLocalPlayer ? "Baymax (Me) " : "Baymax (Enemy)";
-        /*
+		GameObject weapon = (GameObject)Instantiate (Utilities.rand.Next (0, 2) == 0 ? SubmachinegunPrefab : FlamethrowerPrefab, this.transform, false);
+		currentWeapon = weapon.GetComponent<Weapon> ();
+
+		myCamera = GetComponentInChildren<Camera> ();
+
+
+		if (isLocalPlayer) {
+			PlayerController[] allPlayers = GameObject.FindObjectsOfType<PlayerController> ();
+			Respawn newRespawn = null;
+			foreach (var p in allPlayers) {
+				if (p.gameObject != gameObject)
+					newRespawn = Utilities.getNewRespawnPoint (p.gameObject);
+			}
+			if (newRespawn == null)
+				newRespawn = Utilities.getRandomRespawnPoint ();
+
+			var cam = myCamera;
+			Debug.Assert (cam != null);
+			var gun = currentWeapon;
+			Debug.Assert (gun != null);
+
+			newRespawn.setRespawn (gameObject, cam.gameObject, gun.gameObject);
+
+
+
+		}
+		gameObject.name = isLocalPlayer ? "Baymax (Me) " : "Baymax (Enemy)";
+		/*
             if (Network.isServer)
                 gameObject.name = isLocalPlayer ? "Baymax (Server)" : "Baymax (Client)";
             else
                 gameObject.name = isLocalPlayer ? "Baymax (Client)" : "Baymax (Server)";
         */
-    }
+	}
 
-    private void PlayerController_EndKillcam()
-    {
-        Dead = false;
-        Destroy(GetComponentInChildren<Weapon>().gameObject);
-        GameObject weapon = (GameObject)Instantiate(Utilities.rand.Next(0, 2) == 0 ? SubmachinegunPrefab : FlamethrowerPrefab, this.transform, false);
-        currentWeapon = weapon.GetComponent<Weapon>();
-        GetComponent<Health>().CmdRespawn();
-    }
+	private void PlayerController_EndKillcam ()
+	{
+		Dead = false;
+		Destroy (GetComponentInChildren<Weapon> ().gameObject);
+		GameObject weapon = (GameObject)Instantiate (Utilities.rand.Next (0, 2) == 0 ? SubmachinegunPrefab : FlamethrowerPrefab, this.transform, false);
+		currentWeapon = weapon.GetComponent<Weapon> ();
+		GetComponent<Health> ().CmdRespawn ();
+	}
 
-    public override void OnStartLocalPlayer ()
+	public override void OnStartLocalPlayer ()
 	{
         
     
-    }
+	}
 
 	void moveForwardsOrBackwards (float val)
 	{
-        if (Dead) return;
+		if (Dead)
+			return;
 
-        Vector3 vect = new Vector3 (val, 0, 0);
+		Vector3 vect = new Vector3 (val, 0, 0);
 		transform.Translate (vect * moveSpeed * Time.deltaTime);
 	}
 
 	void moveLeftOrRight (float val)
 	{
-        if (Dead) return;
+		if (Dead)
+			return;
 
-        Vector3 vect = new Vector3 (0, 0, val);
+		Vector3 vect = new Vector3 (0, 0, val);
 		transform.Translate (vect * moveSpeed * Time.deltaTime);
 	}
 
     
 	void CmdRotateVertically ()
 	{
-        if (Dead) return;
+		if (Dead)
+			return;
 
-        Camera cam = myCamera;
-        //TODO: idk if we'll be able to generalize this...
-        var gun = transform.Find("SubmachineGun");//currentWeapon;
+		Camera cam = myCamera;
+		//TODO: idk if we'll be able to generalize this...
+		var gun = transform.Find ("SubmachineGun");//currentWeapon;
 		if (currentWeapon.pitch == Constants.INVALID_PITCH) {
-            currentWeapon.pitch = cam.transform.eulerAngles.x;
+			currentWeapon.pitch = cam.transform.eulerAngles.x;
 		}
-        currentWeapon.pitch -= speedV * Input.GetAxis (Utilities.isXboxController () ? "Right joystick vertical" : "Mouse Y");
+		currentWeapon.pitch -= speedV * Input.GetAxis (Utilities.isXboxController () ? "Right joystick vertical" : "Mouse Y");
 
 		cam.transform.eulerAngles = new Vector3 (currentWeapon.pitch, cam.transform.eulerAngles.y, 0.0f);
 		gun.transform.eulerAngles = new Vector3 (gun.transform.eulerAngles.x, gun.transform.eulerAngles.y, currentWeapon.pitch);
-        // Clamp pitch:
-        currentWeapon.pitch = Mathf.Clamp (currentWeapon.pitch, -70f, 70f);
+		// Clamp pitch:
+		currentWeapon.pitch = Mathf.Clamp (currentWeapon.pitch, -70f, 70f);
 
 		cam.transform.eulerAngles = new Vector3 (currentWeapon.pitch, cam.transform.eulerAngles.y, 0f);
 		gun.transform.eulerAngles = new Vector3 (gun.transform.eulerAngles.x, gun.transform.eulerAngles.y, currentWeapon.pitch);
@@ -198,9 +195,10 @@ public abstract class PlayerController : NetworkBehaviour
   
 	void CmdRotateHorizontally ()
 	{
-        if (Dead) return;
+		if (Dead)
+			return;
 
-        if (yaw == -1f) {
+		if (yaw == -1f) {
 			yaw = transform.eulerAngles.y;
 		}
 
@@ -222,23 +220,26 @@ public abstract class PlayerController : NetworkBehaviour
 
 	void jump ()
 	{
-        if (Dead) return;
+		if (Dead)
+			return;
 
-        if (!jumpIsCoolingDown && IsOnGround ()) {
+		if (!jumpIsCoolingDown && IsOnGround ()) {
 			GetComponent<Rigidbody> ().AddForce (Vector3.up * Constants.JUMP_FORCE);
-            jumpCooldownTimer.Enabled = true;
-            jumpIsCoolingDown = true;
+			jumpCooldownTimer.Enabled = true;
+			jumpIsCoolingDown = true;
 		}
 	}
 
 	bool IsOnGround ()
 	{
-        Collider lowestCollider = null;
-        float lowY = float.MaxValue;
-        foreach (var collider in GetComponentsInChildren<Collider>())
-        {
-            if (collider.transform.position.y < lowY) { lowY = collider.transform.position.y;  lowestCollider = collider; }
-        }
+		Collider lowestCollider = null;
+		float lowY = float.MaxValue;
+		foreach (var collider in GetComponentsInChildren<Collider>()) {
+			if (collider.transform.position.y < lowY) {
+				lowY = collider.transform.position.y;
+				lowestCollider = collider;
+			}
+		}
 
 		float raycastDistance = lowestCollider.bounds.extents.y + 0.8f;
 		return Physics.Raycast (transform.position, -Vector3.up, raycastDistance);
@@ -246,8 +247,9 @@ public abstract class PlayerController : NetworkBehaviour
 
 	bool deathByFalling ()
 	{
-        if (Dead) return false;
-		if (transform.position.y > Utilities.GetDeathY(Utilities.GetCurrentScene())) {
+		if (Dead)
+			return false;
+		if (transform.position.y > Utilities.GetDeathY (Utilities.GetCurrentScene ())) {
 			return false;
 		}
 		var health = GetComponent<Health> ();
@@ -255,23 +257,24 @@ public abstract class PlayerController : NetworkBehaviour
 		return true;
 	}
 
-    [Command]
+	[Command]
 	public void CmdFire ()
 	{
-        if (Dead) return;
+		if (Dead)
+			return;
 
-        if (!currentWeapon.CanFire)
-            return;
+		if (!currentWeapon.CanFire)
+			return;
 
-        // Create the Bullet from the Bullet Prefab
-        var canvas = transform.Find("Canvas");
-        var crossHair = canvas.Find("Crosshair");
-        Projectile projectile = currentWeapon.CmdCreateProjectile(crossHair);
-        Destroy(projectile.gameObject, 2f);
-        NetworkServer.Spawn(projectile.gameObject);
-        projectile.RpcSetRotation();
+		// Create the Bullet from the Bullet Prefab
+		var canvas = transform.Find ("Canvas");
+		var crossHair = canvas.Find ("Crosshair");
+		Projectile projectile = currentWeapon.CmdCreateProjectile (crossHair);
+		Destroy (projectile.gameObject, 2f);
+		NetworkServer.Spawn (projectile.gameObject);
+		projectile.RpcSetRotation ();
 
-    }
+	}
 
 	public void updateScoreboard ()
 	{
@@ -284,13 +287,12 @@ public abstract class PlayerController : NetworkBehaviour
    
 
    
-    void Update ()
+	void Update ()
 	{
-        if (!isLocalPlayer) {
-            if(GetComponentInChildren<Camera>() != null)
-            {
-                GetComponentInChildren<Camera>().enabled = false;
-            }
+		if (!isLocalPlayer) {
+			if (GetComponentInChildren<Camera> () != null) {
+				GetComponentInChildren<Camera> ().enabled = false;
+			}
 			
 			return;
 		}
@@ -298,8 +300,8 @@ public abstract class PlayerController : NetworkBehaviour
 		if (deathByFalling ()) {
 			return;
 		}
-        var health = GetComponent<Health>();
-        //Debug.Log("kills: " + health.kills + " deaths: " + health.deaths);
+		var health = GetComponent<Health> ();
+		//Debug.Log("kills: " + health.kills + " deaths: " + health.deaths);
 		CmdRotateHorizontally ();
 		CmdRotateVertically ();
 		if (Utilities.isXboxController ()) {
@@ -312,27 +314,27 @@ public abstract class PlayerController : NetworkBehaviour
 			if (triggerInput > 0) { //left trigger
 				Debug.Log ("Grenade!!!");
 			} else if (triggerInput < 0) { //right trigger
-                CmdFire();
+				CmdFire ();
 			}
 		} else {
-            bool moved = false;
+			bool moved = false;
 			if (Input.GetKey (KeyCode.A)) {
 				moveLeftOrRight (-1);
-                moved = true;
+				moved = true;
 			}
 			if (Input.GetKey (KeyCode.D)) {
 				moveLeftOrRight (1);
-                moved = true;
-            }
+				moved = true;
+			}
 			if (Input.GetKey (KeyCode.W)) {
 				moveForwardsOrBackwards (-1);
-                moved = true;
-            }
+				moved = true;
+			}
 			if (Input.GetKey (KeyCode.S)) {
 				moveForwardsOrBackwards (1);
-                moved = true;
-            }
-            Walking = moved && !Dead;
+				moved = true;
+			}
+			Walking = moved && !Dead;
     
 
 			if (Input.GetKeyUp (KeyCode.Space)) {
@@ -340,22 +342,21 @@ public abstract class PlayerController : NetworkBehaviour
 			}
 			if (Input.GetKey (KeyCode.Mouse0)) {
                
-                var canvas = transform.Find("Canvas");
-                var crossHair = canvas.Find("Crosshair");
-                Vector3 crossHairPosition = crossHair.position;
-                CmdFire ();
+				var canvas = transform.Find ("Canvas");
+				var crossHair = canvas.Find ("Crosshair");
+				Vector3 crossHairPosition = crossHair.position;
+				CmdFire ();
 			}
            
 		}
 
-        if (jumpCooldownTimer.IncrementIfEnabled())
-        {
-            jumpCooldownTimer.Enabled = false;
-            jumpCooldownTimer.Reset();
-            jumpIsCoolingDown = false;
-        }
+		if (jumpCooldownTimer.IncrementIfEnabled ()) {
+			jumpCooldownTimer.Enabled = false;
+			jumpCooldownTimer.Reset ();
+			jumpIsCoolingDown = false;
+		}
 
 
-    }
+	}
 
 }
